3bae2e23e47c4f3476039978e428048cef10b818
Ammar Zahid <ammarzahid304@gmail.com>
2023-07-25 12:25:02 +0500

    work section completed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const cursor = document.getElementById('cursor');
+// const cursor = document.getElementById('cursor');
 
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+// document.addEventListener('mousemove', (e) => {
+//   const x = e.clientX;
+//   const y = e.clientY;
   
-  // Set the position for the chasing circle
-  cursor.style.transform = `translate3d(${x - 3}px, ${y + 1}px, 0)`;
-});
+//   // Set the position for the chasing circle
+//   cursor.style.transform = `translate3d(${x - 3}px, ${y + 87}px, 0)`;
+// });
