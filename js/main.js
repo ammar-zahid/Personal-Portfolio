7e0a8f99828b8937721a80ec979279040f4ad916
Ammar Zahid <ammarzahid304@gmail.com>
2023-08-05 19:50:43 +0500

    Added chasing circles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// chasing circle 1
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cursorCircle1 = document.querySelector('.cursor-circle1');
+    const radius = cursorCircle1.clientWidth / 2;
+  
+    document.addEventListener('mousemove', (e) => {
+      const x = e.clientX - radius;
+      const y = e.clientY - radius;
+      cursorCircle1.style.transform = `translate(${x}px, ${y}px)`;
+    });
+  });  
+
+  // chasing circle 2
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const cursorCircle2 = document.querySelector('.cursor-circle2');
+    const radius = cursorCircle2.clientWidth /2;
+  
+    document.addEventListener('mousemove', (e) => {
+      const x = e.clientX - radius;
+      const y = e.clientY - radius;
+      cursorCircle2.style.transform = `translate(${x}px, ${y}px)`;
+    });
+  });
+  
 (function($) {
     "use strict";
     // mobile Menu area
